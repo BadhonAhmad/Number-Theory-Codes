2571f7032f1f9c7665c6ebf4741def2ba2ed3ab0
Nobel Ahmad Badhon <107022184+BadhonAhmad@users.noreply.github.com>
2023-05-28 10:42:56 +0600

    Create all substrings.cpp

---
+++ all substrings.cpp
@@ -0,0 +1,15 @@
+for ( i = 0; i < s.size(); i++)
+    {
+        for ( j = i+1; j <= s.size(); j++)
+        {
+            string temp="";
+            for ( k = i; k < j; k++)
+            {
+                temp+=s[k];
+                
+            }
+           cout << temp << e;
+
+        }
+        
+    }
