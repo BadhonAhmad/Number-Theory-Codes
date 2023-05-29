312b6eed41fe90b523ab1d316a57a96856634842
Nobel Ahmad Badhon <107022184+BadhonAhmad@users.noreply.github.com>
2023-05-29 17:52:37 +0600

    Create pattern_matching_naive.cpp

---
+++ pattern_matching_naive.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+const int N = 1e5 + 10;
+int32_t main()
+{
+    string s,p;
+    cin >> s >> p;
+    int l1 = s.size(),l2=p.size();
+
+    for (int i = 0; i <= l1 - l2; i++){
+        if(s[i] == p[0]){
+            int j;
+            for (j = 0; j < l2; j++){
+                if(s[i++] == p[j]){
+                    continue;
+                }
+                else break;
+            }
+            if(j == l2){
+                cout << "pattern is found at index =" << i-j+1 << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "pattern is not found" << endl;
+}
