a00e23167731e63df2b574c1d7c81cdce01f7e9b
Nobel Ahmad Badhon <107022184+BadhonAhmad@users.noreply.github.com>
2023-05-29 18:11:02 +0600

    Update pattern_matching_naive.cpp

--- pattern_matching_naive.cpp
+++ pattern_matching_naive.cpp
@@ -1,28 +1,32 @@
 #include<bits/stdc++.h>
+using namespace std;
+
 #define int long long
 #define endl "\n"
-using namespace std;
 
 int32_t main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
     string s,p;
     cin >> s >> p;
-    int l1 = s.size(),l2=p.size();
+    int l1 = s.size(), f = 0, l2=p.size();
 
     for (int i = 0; i <= l1 - l2; i++){
         if(s[i] == p[0]){
             int j;
             for (j = 0; j < l2; j++){
-                if(s[i++] == p[j]){
-                    continue;
-                }
-                else break;
+                if (s[i + j] != p[j])
+                break;
             }
             if(j == l2){
-                cout << "pattern is found at index =" << i-j+1 << endl;
-                return 0;
+                f = 1;
+                cout << "pattern found at index " << i << endl;
             }
         }
-    }//complexity=O(m*(n-m))
+    }
+    if(!f)
     cout << "pattern is not found" << endl;
+    ///complexity:O(l2*(l1-l2))
 }
