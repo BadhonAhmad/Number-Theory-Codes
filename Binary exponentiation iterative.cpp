e6a9c5722247e20d99e3888956d5e5581e8d612c
Nobel Ahmad Badhon <107022184+BadhonAhmad@users.noreply.github.com>
2023-08-29 14:03:58 +0600

    Create Binary exponentiation iterative.cpp

---
+++ Binary exponentiation iterative.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int M = 1e9 + 7;
+
+int binExpIt(int a, int b)
+{
+    int ans = 1;
+    while (b)
+    {
+        if (b & 1)
+        {
+            ans = (ans * a) % M;
+        }
+        b >>= 1;
+        a = (a * a) % M;
+    }
+    return ans;
+}
+int32_t main()
+{
+    int a = 2, b = 13;
+    cout << binExpIt(a, b) << endl;
+    return 0;
+}
