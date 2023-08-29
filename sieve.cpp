9d0ca4ebf77531f5ee2f502b50d7057d69262626
Nobel Ahmad Badhon <107022184+BadhonAhmad@users.noreply.github.com>
2023-08-29 22:58:26 +0600

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -1,39 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 #define endl "\n"
 #define int long long
 const int N=1e7+10;
-int is_prime[N];
-vector<int>prime;
 
-void primeGen ( int n ) // give sqroot of given n while computing prime facto of n;
-{
-    n += 10;
-    for ( int i = 3; i <= n; i += 2 ) is_prime[i] = 1;
- 
-    int sq = sqrt ( n );
-    for ( int i = 3; i <= sq; i += 2 ) {
-        if ( is_prime[i] == 1 ) {
-            for ( int j = i*i; j <= n; j += ( i + i ) ) {
-                is_prime[j] = 0;
-            }
-        }
-    }
- 
-    is_prime[2] = 1;
-    prime.push_back (2);
- 
-    for ( int i = 3; i <= n; i += 2 ) {
-        if ( is_prime[i] == 1 ) prime.push_back ( i );
-    }
-}
-
+vector<bool>isPrime(N,1);
 int32_t main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n; cin>>n;
-    primeGen(n);
-    for(auto it:prime) cout<<it<<' ';
+    isPrime[0]=isPrime[1]=false;
+    for(int i=2; i<N; i++){
+        if(isPrime[i] ==true){
+            for(int j=2*i; j<N; j+=i){
+                isPrime[j]=false;
+            }
+        }
+    }
+    int t=1; cin>>t;
+    while(t--){
+        
+    }
+
     return 0;
 }
